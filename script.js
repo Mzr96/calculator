8888c82e11c8d5893142216e66b05d433c5b0ace
Maziar <maziarmoslemi@gmail.com>
2022-07-29 12:04:35 +0430

    Create 4 functions for main math operation
    add, subtract, divide, multiply

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const add = (num1, num2) => num1 + num2;
+const subtract = (num1, num2) => num1 - num2;
+const divide = (num1, num2) => num1 / num2;
+const multiply = (num1, num2) => num1 * num2;
