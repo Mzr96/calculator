79fb19d3205d2a2c770495eaecb1fb1a42536472
Maziar <maziarmoslemi@gmail.com>
2022-07-29 12:19:20 +0430

    Create operate function

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const divide = (num1, num2) => num1 / num2;
 const multiply = (num1, num2) => num1 * num2;
+
+const operate = function (operator, num1, num2) {
+  if (operator === "+") {
+    add(num1, num2);
+  } else if (operator === "-") {
+    subtract(num1, num2);
+  } else if (operator === "/") {
+    divide(num1, num2);
+  } else if (operator === "*") {
+    multiply(num1, num2);
+  }
+};
