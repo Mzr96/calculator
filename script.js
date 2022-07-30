0772673480b12545c7ed34f8a7014152165b1c78
Maziar <maziarmoslemi@gmail.com>
2022-07-30 10:54:26 +0430

    Add listener to digit and operator buttons
    -Digit buttons update the display to show the current number or result.

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const add = (num1, num2) => num1 + num2;
 const subtract = (num1, num2) => num1 - num2;
 const divide = (num1, num2) => num1 / num2;
 const multiply = (num1, num2) => num1 * num2;
+const inputs = {};
+const display = document.querySelector(".display");
+const btnDigits = Array.from(document.querySelectorAll(".digit"));
+const btnOperators = Array.from(document.querySelectorAll(".operator"));
+
+btnDigits.forEach((btnDigit) =>
+  btnDigit.addEventListener("click", function () {
+    display.textContent += btnDigit.textContent;
+  })
+);
+console.log(btnOperators);
+btnOperators.forEach((btn) =>
+  btn.addEventListener("click", function (e) {
+    const numberOnDisplay = Number(display.textContent);
+    const operator = e.target.textContent;
+    inputs.fNumber = numberOnDisplay;
+    inputs.operator = operator;
+    display.textContent = "";
+  })
+);
 
 const operate = function (operator, num1, num2) {
   if (operator === "+") {
