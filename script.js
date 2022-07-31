165fd6e8d5eba417321aff1c564aceaa8832b788
Maziar <maziarmoslemi@gmail.com>
2022-07-31 12:53:44 +0430

    Fix bugs and add some extra features
    Fixed Bugs:
    -Euqual sign doesn't work before entering seconde number
    -Pressing button after result number are not going to add to display.
    -Chaining calculation problem is solved.
    -Clear button is functional.
    -Rounding decimal numbers
    
    New Feature:
    -Add decimal point button.

--- script.js
+++ script.js
@@ -5,38 +5,86 @@ const subtract = (num1, num2) => num1 - num2;
 const divide = (num1, num2) => num1 / num2;
 const multiply = (num1, num2) => num1 * num2;
 let inputs = {};
+let isEraserOn = false;
 const display = document.querySelector(".display");
 const btnDigits = Array.from(document.querySelectorAll(".digit"));
 const btnOperators = Array.from(document.querySelectorAll(".operator"));
 const btnEqual = document.querySelector(".equal");
+const btnClear = document.querySelector(".clear");
+const btnDecimalPoint = document.querySelector(".decimal");
+
+btnClear.addEventListener("click", function () {
+  reset();
+});
+
+btnDecimalPoint.addEventListener("click", function () {
+  if (display.textContent.indexOf(".") === -1) {
+    if (isEraserOn) {
+      display.textContent = "0.";
+      isEraserOn = false;
+    } else {
+      display.textContent += ".";
+    }
+  } else if (isEraserOn) {
+    display.textContent = "0.";
+    isEraserOn = false;
+  }
+});
 
 btnDigits.forEach((btnDigit) =>
   btnDigit.addEventListener("click", function () {
-    if (
-      inputs.fNumber === Number(display.textContent) ||
-      inputs.sNumber === Number(display.textContent)
-    ) {
-      display.textContent = "";
+    // console.log(inputs);
+
+    // if (
+    //   inputs.fNumber === Number(display.textContent) ||
+    //   inputs.sNumber === Number(display.textContent) ||
+    //   inputs.result === Number(display.textContent)
+    // ) {
+    //   display.textContent = "";
+    //   // console.log(inputs);
+    // }
+    // display.textContent += btnDigit.textContent;
+    if (isEraserOn) {
+      display.textContent = btnDigit.textContent;
+      isEraserOn = false;
+    } else {
+      display.textContent += btnDigit.textContent;
     }
-    display.textContent += btnDigit.textContent;
   })
 );
 
 btnOperators.forEach((btnOperator) =>
   btnOperator.addEventListener("click", function (e) {
-    const numberOnDisplay = Number(display.textContent);
-    const operator = e.target.textContent;
-    inputs.fNumber = numberOnDisplay;
-    inputs.operator = operator;
+    console.log(inputs);
+    // const numberOnDisplay = Number(display.textContent);
+    // const operator = e.target.textContent;
+    // inputs.fNumber = numberOnDisplay;
+    // inputs.operator = operator;
     // display.textContent = "";
+    isEraserOn = true;
+    if (!inputs.sNumber && inputs.fNumber && display.textContent) {
+      calc();
+      inputs.fNumber = Number(display.textContent);
+    } else if (!inputs.fNumber && display.textContent) {
+      inputs.fNumber = Number(display.textContent);
+    }
+    inputs.operator = e.target.textContent;
+    console.log(inputs);
   })
 );
 
 btnEqual.addEventListener("click", function () {
-  const numberOnDisplay = Number(display.textContent);
-  inputs.sNumber = numberOnDisplay;
-  const result = operate(inputs.operator, inputs.fNumber, inputs.sNumber);
-  display.textContent = result;
+  // const numberOnDisplay = Number(display.textContent);
+  // inputs.sNumber = numberOnDisplay;
+  // const result = operate(inputs.operator, inputs.fNumber, inputs.sNumber);
+  // inputs.result = result;
+  // display.textContent = result;
+  // delete inputs.sNumber;
+  // console.log(inputs);
+  if (inputs.fNumber && inputs.operator) {
+    isEraserOn = true;
+    calc();
+  }
 });
 
 const operate = function (operator, num1, num2) {
@@ -51,4 +99,16 @@ const operate = function (operator, num1, num2) {
   }
 };
 
-const calc = function () {};
+const calc = function () {
+  inputs.sNumber = Number(display.textContent);
+  console.table(inputs);
+  const result = operate(inputs.operator, inputs.fNumber, inputs.sNumber);
+  display.textContent = Math.round(result * 100) / 100;
+  inputs = {};
+};
+
+const reset = function () {
+  inputs = {};
+  display.textContent = "";
+  isEraserOn = false;
+};
